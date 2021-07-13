d064524e22311528666f0cddde93d76272350794
kimdohyun <azzadh999@gmail.com>
2021-07-13 01:13:08 +0000

    first build

---
+++ history.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y nodejs
+
+echo ""
+echo ""
+echo "Your Node Version:"
+node -v
+echo ""
+echo ""
+
+sleep 1
+echo "."
+sleep 2
+echo "."
+sleep 3
+echo "."
+
+sudo apt-get install -y npm
+
+echo ""
+echo ""
+echo "Your Npm Version:"
+npm -v
+echo ""
+echo ""
+
+sudo npm install -g express
+sudo npm install -g express-generator
