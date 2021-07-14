18b8ad4fadf0547030e90457a4540642af7bd7f8
azza999 <azzadh999@gmail.com>
2021-07-14 14:59:30 -0700

    add qr-read

--- public/scripts/read-qr.js
+++ public/scripts/read-qr.js
@@ -1,20 +1,43 @@
 const Reader = {
 	init: function() {
 		this.video = document.getElementById('video')
+		this.cvs = document.getElementById('cvs')
+		this.ctx = this.cvs.getContext('2d')
 		this.$status = document.getElementById('status')
+
+		this.videoInterval = null
+
 		this.stream = null
 		this.allowed = false
 	},
+
 	active: async function () {
 		await this.requestCamera()
+
+		this.startVideo2cvs()
 
 		if (this.allowed === true) {
 			this.video.srcObject = this.stream
 			this.video.play()
 		} else {
-			
+			this.$status.innerHTML = 'cannot use video'
 		}
 	},
+
+	startVideo2cvs: function() {
+		this.videoInterval = setInterval(_=>{
+			this.ctx.drawImage(this.video, 0, 0, this.video.width, this.video.height)
+
+			let result = jsqr(this.ctx.ImageData(), this.video.width, this.video.height)
+
+			this.$status.innerHTML = result
+		},20)
+	},
+
+	stopVideo2cvs: function() {
+		clearInterval(this.videoInterval)
+	},
+
 	requestCamera: async function() {
 		await navigator.mediaDevices.getUserMedia({video: true})
 		.then( mediaStream=>{
@@ -23,7 +46,6 @@ const Reader = {
 		})
 		.catch( err => {
 			console.log(err)
-			$status.innerHTML = 'cannot use video'
 		})
 	}
 }
